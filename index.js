1addfb31ac20ccb1afbc064f81cd48e4e71d6497
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-09 17:07:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+
+function theBeatlesPlay(arrayM, arrayI) {
+  
+  let empty = []
+  
+  for (i = 0; i < arrayM.length ; i++) {
+    
+    empty.push(`${arrayM[i]} plays ${arrayI[i]}`)
+  }
+  
+  return empty
+}
